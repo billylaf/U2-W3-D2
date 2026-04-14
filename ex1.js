01d5f95ef0912781cd4ece9b29517453dd663fe9
bilal lafdili <bilallafdili23@gmail.com>
2026-04-14 15:04:20 +0200

    Update ex1.js

--- ex1.js
+++ ex1.js
@@ -22,3 +22,13 @@ const reset = function () {
   textarea.value = ""
   localStorage.removeItem(KEY)
 }
+
+/* metodo per incerementare secondi alla pagina senza resettarla alla fine della sessiona */
+/* const timer = document.getElementById("timer");
+let count = parseInt(sessionStorage.getItem("timer")) || 0;
+timer.textContent = count;
+setInterval(() => {
+  count++;
+  sessionStorage.setItem("timer", count);
+  timer.textContent = count;
+}, 1000); */
